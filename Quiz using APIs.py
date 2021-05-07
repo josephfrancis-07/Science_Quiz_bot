0e9eac15949deb96aade3f1c4385c171a97e5fe8
JOSEPH FRANCIS PELLISSERY <66055742+josephfrancis-07@users.noreply.github.com>
2021-05-07 21:11:24 +0530

    Update Quiz using APIs.py

--- Quiz using APIs.py
+++ Quiz using APIs.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import random as rand
+import html
 
 data_validation3=False
 while data_validation3==False:
@@ -11,19 +12,19 @@ while data_validation3==False:
         category= string['results'][0]['category']
         type_=string['results'][0]['type']
         level=string['results'][0]['difficulty']
-        question=string['results'][0]['question']
+        question=html.unescape(string['results'][0]['question'])
      
     #generating list of options and inserting the correct answer into a random position
     
         options=[]
-        options.append(string['results'][0]['incorrect_answers'][0])
-        options.append(string['results'][0]['incorrect_answers'][1])
-        options.append(string['results'][0]['incorrect_answers'][2])
-        options.insert((rand.randint(0, 3)),string['results'][0]['correct_answer'])
+        options.append(html.unescape(string['results'][0]['incorrect_answers'][0]))
+        options.append(html.unescape(string['results'][0]['incorrect_answers'][1]))
+        options.append(html.unescape(string['results'][0]['incorrect_answers'][2]))
+        options.insert((rand.randint(0, 3)),html.unescape(string['results'][0]['correct_answer']))
     
     #printing the question and options
     
-        print("\nCategory : ",category,"\t\tLevel : ",level,"\n\n Question : ",question,"\n\n\tOptions : 1) ",options[0],"\n\t          2)",options[1],"\n\t          3)",options[2],"\n\t          4)",options[3],"\n")
+        print("\nCategory : ",category,"\t\tLevel : ",level,"\n\n Question : ",question,"\n\n\tOptions : 1)",options[0],"\n\t          2)",options[1],"\n\t          3)",options[2],"\n\t          4)",options[3],"\n")
         
     #inputing data , validatin and error handling are doing here.
         
@@ -79,4 +80,5 @@ while data_validation3==False:
             continue
             
         
+      
     
